bacfb2b5a0406c18f4e2158169b53dc30b73fe0e
sayd24 <muhammadsayyidtsabit@gmail.com>
2026-09-22 22:53:26 +0700

    Update 1-Intro.py

--- Live code PCV/1-Intro.py
+++ Live code PCV/1-Intro.py
@@ -1,23 +1,23 @@
 import cv2
-from matplotlib import pyplot as plt
+import numpy as np
 
-img= cv2.imread("grha_its.jpg")
-[h, w, c]= img.shape
+img = cv2.imread("grha_its.jpg")
+[h, w, c] = img.shape
 
-imgMerah= img.copy()
-imgHijau= img.copy()
-imgBiru= img.copy()
+imgMerah = img.copy()
+imgHijau = img.copy()
+imgBiru = img.copy()
 
 for i in range(h):
     for j in range(w):
-        imgMerah[i,j,0]= 0
-        imgMerah[i,j,1]= 0
+        imgMerah[i,j,0] = 0
+        imgMerah[i,j,1] = 0
         
-        imgHijau[i,j,0]= 0
-        imgHijau[i,j,2]= 0
+        imgHijau[i,j,0] = 0
+        imgHijau[i,j,2] = 0
         
-        imgBiru[i,j,1]= 0
-        imgBiru[i,j,2]= 0
+        imgBiru[i,j,1] = 0
+        imgBiru[i,j,2] = 0
 
 cv2.imshow("Image Asli", img)
 cv2.imshow("Image Merah", imgMerah)
@@ -35,22 +35,25 @@ while True:
     if not ret:
         break
         
-    cv2.imshow("Video Asli", frame)
-    
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+        
     frameMerah = frame.copy()
-    frameMerah[:,:,0]= 0
-    frameMerah[:,:,1]= 0
-    cv2.imshow("Video Merah", frameMerah)
+    frameMerah[:,:,0] = 0
+    frameMerah[:,:,1] = 0
     
     frameHijau = frame.copy()
-    frameHijau[:,:,0]= 0
-    frameHijau[:,:,2]= 0
-    cv2.imshow("Video Hijau", frameHijau)
+    frameHijau[:,:,0] = 0
+    frameHijau[:,:,2] = 0
     
     frameBiru = frame.copy()
-    frameBiru[:,:,1]= 0
-    frameBiru[:,:,2]= 0
-    cv2.imshow("Video Biru", frameBiru)
+    frameBiru[:,:,1] = 0
+    frameBiru[:,:,2] = 0
+    
+    baris_atas = np.hstack((frame, frameMerah))
+    baris_bawah = np.hstack((frameHijau, frameBiru))
+    frame_gabungan = np.vstack((baris_atas, baris_bawah))
+    
+    cv2.imshow("Live Video - 4 Mode Warna", frame_gabungan)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
